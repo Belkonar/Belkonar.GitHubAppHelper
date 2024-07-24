cbbfa1c78ff3c910e547a5662f3ba1c72d6a57de
Tom <JosephDotson@belkonar.com>
2024-07-24 17:09:56 -0400

    simplify release

--- release.sh
+++ release.sh
@@ -2,6 +2,5 @@
 
 set -e
 
-cd Belkonar.GitHubAppHelper/
-dotnet pack -o out -c Release /p:Version=$1
-dotnet nuget push out/Belkonar.GitHubAppHelper.$1.nupkg --api-key $NUGET_TOKEN --source https://api.nuget.org/v3/index.json
+dotnet pack ./Belkonar.GitHubAppHelper -o out -c Release /p:Version=$1
+dotnet nuget push ./out/Belkonar.GitHubAppHelper.$1.nupkg --api-key $NUGET_TOKEN --source https://api.nuget.org/v3/index.json
