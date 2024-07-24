ca51ee9c5237085c1549e954c540b8266ec1e29b
Tom <JosephDotson@belkonar.com>
2024-07-24 08:37:23 -0400

    fixed props

--- Belkonar.GitHubAppHelper/GitHubAppConfig.cs
+++ Belkonar.GitHubAppHelper/GitHubAppConfig.cs
@@ -1,12 +1,12 @@
 namespace Belkonar.GitHubAppHelper;
 
-// Generally I'd use only init propers but IOptions is dumb.
+// Generally I'd use only init props but IOptions is dumb.
 public class GitHubAppConfig
 {
     public required string AppId { get; set; }
     public string GitHubUri { get; set; } = "https://api.github.com";
     
     public string? Organization { get; set; }
-    public string? Repository { get; init; }
+    public string? Repository { get; set; }
     public string? TokenEnvironmentVariable { get; set; }
 }
